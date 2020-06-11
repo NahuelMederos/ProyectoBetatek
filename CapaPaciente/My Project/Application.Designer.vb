c0abc4ac8be6cc3da7f8725b58121b442bdb8a58
guillermo <guille.bozza@gmail.com>
2020-06-11 13:55:59 -0300

    Agregado a la CapaPaciente 2 frm: IngresoPersona y SeleccionSintoma. FrmSeleccionSintoma se agrego un listbox,un label y un boton de confirmar(todo incompleto)

--- CapaPaciente/My Project/Application.Designer.vb
+++ CapaPaciente/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.CapaPaciente.Form1
+            Me.MainForm = Global.CapaPaciente.SeleccionSintoma
         End Sub
     End Class
 End Namespace
---
+++ CapaPaciente/SeleccionSintoma.vb
@@ -0,0 +1,3 @@
+﻿Public Class SeleccionSintoma
+
+End Class
