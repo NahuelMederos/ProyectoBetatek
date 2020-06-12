3396044ee3e79b8579d928eb42dbc74d02687204
Chadousito <nahuelmederos1@gmail.com>
2020-06-11 21:36:50 -0300

    Login y ventana de gestion

--- Capa Gestion/Form1.Designer.vb
+++ Capa Gestion/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FormLogin
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,12 +20,63 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.btnIngresar = New System.Windows.Forms.Button()
+        Me.FileSystemWatcher1 = New System.IO.FileSystemWatcher()
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(5, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(203, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Bienvenido, ingrese la contraseña de root"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(52, 51)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(100, 20)
+        Me.txtPassword.TabIndex = 1
+        '
+        'btnIngresar
+        '
+        Me.btnIngresar.Location = New System.Drawing.Point(67, 77)
+        Me.btnIngresar.Name = "btnIngresar"
+        Me.btnIngresar.Size = New System.Drawing.Size(75, 23)
+        Me.btnIngresar.TabIndex = 2
+        Me.btnIngresar.Text = "Ingresar"
+        Me.btnIngresar.UseVisualStyleBackColor = True
+        '
+        'FileSystemWatcher1
+        '
+        Me.FileSystemWatcher1.EnableRaisingEvents = True
+        Me.FileSystemWatcher1.SynchronizingObject = Me
+        '
+        'FormLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(220, 106)
+        Me.Controls.Add(Me.btnIngresar)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "FormLogin"
+        Me.Text = "Login"
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents btnIngresar As Button
+    Friend WithEvents FileSystemWatcher1 As IO.FileSystemWatcher
 End Class
--- Capa Gestion/Form1.vb
+++ Capa Gestion/Form1.vb
@@ -1,3 +1,19 @@
-﻿Public Class Form1
+﻿Imports System.Data.Odbc
+Public Class FormLogin
+
+    Dim pass As String
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnIngresar.Click
+        Try
+            pass = txtPassword.Text
+            Dim conexion As New OdbcConnection("DRIVER=MySQL ODBC 8.0 UNICODE Driver;UID=root;PWD=" + pass + ";PORT=3306;DATABASE=betatek;SERVER=localhost")
+            conexion.Open()
+            MsgBox("Se logro la conexion")
+            FormGestion.Visible = True
+            Me.Hide()
+        Catch
+            MsgBox("Contraseña incorrecta")
+        End Try
+    End Sub
 
 End Class
