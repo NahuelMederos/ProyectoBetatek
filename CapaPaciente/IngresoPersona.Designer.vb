8fefed39921a81547b4f62123421e36cec9a020a
guillermo <guille.bozza@gmail.com>
2020-06-14 13:50:38 -0300

    Agregado de labels en IngresoPersonas.

--- CapaPaciente/IngresoPersona.Designer.vb
+++ CapaPaciente/IngresoPersona.Designer.vb
@@ -32,85 +32,196 @@ Partial Class IngresoPersona
         Me.txtEdad = New System.Windows.Forms.TextBox()
         Me.txtMail = New System.Windows.Forms.TextBox()
         Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'TxtNombre
         '
-        Me.TxtNombre.Location = New System.Drawing.Point(455, 84)
+        Me.TxtNombre.Location = New System.Drawing.Point(94, 78)
         Me.TxtNombre.Name = "TxtNombre"
         Me.TxtNombre.Size = New System.Drawing.Size(159, 20)
         Me.TxtNombre.TabIndex = 0
         '
         'txtApellido
         '
-        Me.txtApellido.Location = New System.Drawing.Point(456, 110)
+        Me.txtApellido.Location = New System.Drawing.Point(95, 104)
         Me.txtApellido.Name = "txtApellido"
         Me.txtApellido.Size = New System.Drawing.Size(159, 20)
         Me.txtApellido.TabIndex = 1
         '
         'txtCi
         '
-        Me.txtCi.Location = New System.Drawing.Point(455, 136)
+        Me.txtCi.Location = New System.Drawing.Point(94, 130)
         Me.txtCi.Name = "txtCi"
         Me.txtCi.Size = New System.Drawing.Size(159, 20)
         Me.txtCi.TabIndex = 2
         '
         'txtCalle
         '
-        Me.txtCalle.Location = New System.Drawing.Point(456, 162)
+        Me.txtCalle.Location = New System.Drawing.Point(443, 78)
         Me.txtCalle.Name = "txtCalle"
         Me.txtCalle.Size = New System.Drawing.Size(159, 20)
         Me.txtCalle.TabIndex = 3
         '
         'txtEsquina
         '
-        Me.txtEsquina.Location = New System.Drawing.Point(456, 188)
+        Me.txtEsquina.Location = New System.Drawing.Point(443, 104)
         Me.txtEsquina.Name = "txtEsquina"
         Me.txtEsquina.Size = New System.Drawing.Size(159, 20)
         Me.txtEsquina.TabIndex = 4
         '
         'txtNumero
         '
-        Me.txtNumero.Location = New System.Drawing.Point(456, 214)
+        Me.txtNumero.Location = New System.Drawing.Point(443, 130)
         Me.txtNumero.Name = "txtNumero"
         Me.txtNumero.Size = New System.Drawing.Size(159, 20)
         Me.txtNumero.TabIndex = 5
         '
         'txtTel
         '
-        Me.txtTel.Location = New System.Drawing.Point(456, 240)
+        Me.txtTel.Location = New System.Drawing.Point(94, 167)
         Me.txtTel.Name = "txtTel"
         Me.txtTel.Size = New System.Drawing.Size(159, 20)
         Me.txtTel.TabIndex = 6
         '
         'txtEdad
         '
-        Me.txtEdad.Location = New System.Drawing.Point(456, 266)
+        Me.txtEdad.Location = New System.Drawing.Point(94, 193)
         Me.txtEdad.Name = "txtEdad"
         Me.txtEdad.Size = New System.Drawing.Size(159, 20)
         Me.txtEdad.TabIndex = 7
         '
         'txtMail
         '
-        Me.txtMail.Location = New System.Drawing.Point(456, 292)
+        Me.txtMail.Location = New System.Drawing.Point(94, 219)
         Me.txtMail.Name = "txtMail"
         Me.txtMail.Size = New System.Drawing.Size(159, 20)
         Me.txtMail.TabIndex = 8
         '
         'btnGuardar
         '
-        Me.btnGuardar.Location = New System.Drawing.Point(455, 328)
+        Me.btnGuardar.Location = New System.Drawing.Point(95, 267)
         Me.btnGuardar.Name = "btnGuardar"
         Me.btnGuardar.Size = New System.Drawing.Size(159, 37)
         Me.btnGuardar.TabIndex = 9
         Me.btnGuardar.Text = "Guardar"
         Me.btnGuardar.UseVisualStyleBackColor = True
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(41, 85)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Nombre"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(41, 111)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Apellido"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(41, 137)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(17, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "CI"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(396, 85)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(30, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Calle"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(396, 111)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(45, 13)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Esquina"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(396, 137)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(44, 13)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Numero"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(39, 167)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(49, 13)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "Telefono"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(41, 196)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(32, 13)
+        Me.Label8.TabIndex = 17
+        Me.Label8.Text = "Edad"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(41, 222)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(26, 13)
+        Me.Label9.TabIndex = 18
+        Me.Label9.Text = "Mail"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(458, 28)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(128, 31)
+        Me.Label10.TabIndex = 19
+        Me.Label10.Text = "Direccion"
+        '
         'IngresoPersona
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(703, 467)
+        Me.ClientSize = New System.Drawing.Size(728, 371)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.btnGuardar)
         Me.Controls.Add(Me.txtMail)
         Me.Controls.Add(Me.txtEdad)
@@ -138,4 +249,14 @@ Partial Class IngresoPersona
     Friend WithEvents txtEdad As TextBox
     Friend WithEvents txtMail As TextBox
     Friend WithEvents btnGuardar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
 End Class
