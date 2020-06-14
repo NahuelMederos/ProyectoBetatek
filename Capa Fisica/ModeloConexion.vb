9e6fba62cc09e143ac8496037019fabfc3e2e9ed
guillermo <guille.bozza@gmail.com>
2020-06-14 14:29:24 -0300

    Agregado metodo AltaPersona() y modelo Persona completo.

---
+++ Capa Fisica/ModeloConexion.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Data.Odbc
+
+
+Public MustInherit Class ModeloConexion
+
+    Public DbDriver As String = "MySQL ODBC 8.0 ANSI Driver"
+    Public DbUser As String = "root"
+    Public dbPassword As String = "TooRoot123654"
+    Public DbName As String = ""
+    Public dbHost As String = "LocalHost"
+
+    Public Conexion As New OdbcConnection("DRIVER =" + Me.DbDriver +
+                                           ";UID = " + Me.DbUser +
+                                           ";PWD = " + Me.dbPassword +
+                                           ";DATABASE = " + Me.DbName +
+                                           ";SERVER = " + Me.dbHost)
+
+    Public Comando As New OdbcCommand
+    Public Reader As OdbcDataReader
+
+
+    Public Sub New()
+        Me.conexion.Open()
+        Me.Comando.Connection = Me.Conexion
+
+    End Sub
+
+
+End Class
--- Capa Fisica/Modelos/Persona.vb
+++ Capa Fisica/Modelos/Persona.vb
@@ -1,3 +1,21 @@
 ﻿Public Class Persona
+    Inherits ModeloConexion
+
+    Public Ci As String
+    Public Nombre As String
+    Public Apellido As String
+    Public Calle As String
+    Public Esquina As String
+    Public Numero As String
+    Public Edad As String
+    Public Mail As String
+
+    Public Sub AltaPersona()
+        Comando.CommandText = "INSERT INTO Persona (" + Me.Ci + ",'" + Me.Nombre + "','" + Me.Apellido + "','" + Me.Calle + "','" + Me.Esquina + "'," + Me.Numero + "," + Me.Edad + ",'" + Me.Mail + "')"
+
+        Comando.ExecuteNonQuery()
+
+    End Sub
+
 
 End Class
