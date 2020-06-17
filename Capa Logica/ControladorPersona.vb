f8ab85018b332b05f697a6c4b415e6ace0fcce97
guillermo <guille.bozza@gmail.com>
2020-06-17 14:17:56 -0300

    agregado boton guardar en IngresoPersonas, funciones y sub en sus respectivas capas

--- Capa Logica/ControladorPersona.vb
+++ Capa Logica/ControladorPersona.vb
@@ -1,3 +1,24 @@
-﻿Module ControladorPersona
+﻿Imports Capa_Fisica
+
+
+Module ControladorPersona
+
+
+    Public Sub DarAlta(ci As String, nombre As String, apellido As String, calle As String, esquina As String, numero As String, edad As String, mail As String)
+        Dim p As New Persona
+        p.Ci = ci
+        p.Nombre = nombre
+        p.Apellido = apellido
+        p.Calle = calle
+        p.Esquina = esquina
+        p.Numero = numero
+        p.Edad = edad
+        p.Mail = mail
+
+        p.AltaPersona()
+    End Sub
+
+
+
 
 End Module
--- CapaPaciente/IngresoPersona.vb
+++ CapaPaciente/IngresoPersona.vb
@@ -1,3 +1,13 @@
-﻿Public Class IngresoPersona
+﻿Imports Capa_Logica
 
+
+Public Class IngresoPersona
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        Try
+
+            MsgBox("Persona creada")
+        Catch ex As Exception
+            MsgBox("Hubo un error")
+        End Try
+    End Sub
 End Class
